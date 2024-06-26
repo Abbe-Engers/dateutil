ec1976344d3abb84545db36dd3f2154dafe97146
Yasar1453 <y.s.b.kocdas@student.vu.nl>
2024-06-26 11:34:16 +0200

    Changes for function coverage contains rrule

--- tests/test_rrule.py
+++ tests/test_rrule.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from datetime import datetime, date
 import unittest
 from six import PY2
-
+from dateutil.rrule import rrule, DAILY
 from dateutil import tz
 from dateutil.rrule import (
     rrule, rruleset, rrulestr,
@@ -4912,3 +4912,20 @@ class WeekdayTest(unittest.TestCase):
 
         for repstr, wday in zip(with_n_reprs, with_n_wdays):
             self.assertEqual(repr(wday), repstr)
+
+    def test_contains_elif_branch(self):
+        from datetime import datetime, timedelta
+        from dateutil.rrule import rrule, DAILY
+
+        # Create an rrule object with a known set of elements
+        rr = rrule(DAILY, count=3, dtstart=datetime(1997, 9, 2, 9, 0))
+
+        # Iterate once to make sure the cache is partially filled but not complete
+        next(iter(rr))
+        
+        # Check for an item that is smaller than the first element to trigger the elif branch
+        self.assertFalse(datetime(1997, 9, 1, 9, 0) in rr)
+
+    # Make sure to include this test in your existing test class
+    if __name__ == '__main__':
+        unittest.main()
